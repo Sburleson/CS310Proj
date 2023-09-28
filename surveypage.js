b30bb4ea3ca795040520a75bc20e71ebe8b2419d
Sburleson <simonb9th@gmail.com>
2023-09-28 15:29:49 -0400

    bunch of updates to survey

--- surveypage.js
+++ surveypage.js
@@ -1,28 +1,38 @@
-// check credentials of login
-(function () {
-    window.onload = initialization;
 
+// check credentials of login
+"use strict";
+(function () {
+    window.addEventListener('load',initialization);
     function initialization() {
+        console.log("init");
         let Sid = document.getElementById('StudentID');
         let pwd = document.getElementById('pwd');
         let SubBtn = document.getElementById('logbtn');
 
-        SubBtn.addEventListener('click', checkCred);
+        SubBtn.addEventListener('click', test);
+        console.log(SubBtn);
+    }
 
+    function test(){
+        console.log("out");
+    }
         function checkCred() {
+            console.log("checkCred");
+            // setting cred to true just for now
+            let cred = true;
+            if (cred){
+                let LogDiv = document.getElementById('login');
+                let DropDiv= document.getElementById('dropdown');
+                console.log(DropDiv);
+                DropDiv.classList.remove('visible');
+                
+            }
             // Retrieve the values of StudentID and password fields
-            let studentIdValue = Sid.value;
-            let pwdValue = pwd.value;
+            //let studentIdValue = Sid.value;
+           // let pwdValue = pwd.value;
 
             //check database or json file idk yet.
             //since we seem to be uisng a static folder i would guess json file
-            if (cred){
-                let LogDiv = getElementById('login');
-
-                LogDiv.classList.add('loggedin');
-                LogDiv.classList.remove('not-loggedin');
-                
-            }
+            
         }
-    }
 })();
