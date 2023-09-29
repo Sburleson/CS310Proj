fe6ec12430b6fb6cb82d202d0514a26224e50788
Sburleson <simonb9th@gmail.com>
2023-09-28 23:32:58 -0400

    put things in folders, made changes to reshall page

---
+++ reshalls/ResHalls.js
@@ -0,0 +1,15 @@
+(function () {
+
+window.addEventListener('load',initialization)
+
+    function initialization() {
+        console.log("init");
+        let dropdown = document.getElementById('dropdown');
+
+        dropdown.addEventListener('click', () => {
+            this.selected
+        });
+        console.log(dropdown);
+    }
+
+})();
