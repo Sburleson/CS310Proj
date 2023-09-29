81708ea2ab64743c19f837f4a0f060e6dbcfc00c
Nolan Pettit <pettitn@etown.edu>
2023-09-29 10:22:49 -0400

    updating files

--- reshalls/ResHalls.js
+++ reshalls/ResHalls.js
@@ -2,14 +2,35 @@
 
 window.addEventListener('load',initialization)
 
+    // function initialization() {
+    //     console.log("init");
+    //     let dropdown = document.getElementById('dropdown');
+
+    //     dropdown.addEventListener('click', () => {
+    //         this.selected;
+    //     });
+    //     console.log(dropdown);
+    // }
+
     function initialization() {
         console.log("init");
         let dropdown = document.getElementById('dropdown');
 
-        dropdown.addEventListener('click', () => {
-            this.selected
+        dropdown.addEventListener('change', () => {
+            // Get the selected option value
+            let selectedBuilding = dropdown.value;
+            
+            // Remove the 'hide' class from an element with the id 'founders' when 'Founders' is selected
+            if (selectedBuilding === 'Founders') {
+                let Founders = document.querySelectorAll('Founders');
+                Founders.classList.remove('hide');
+                
+                
+            }
+
+            console.log(dropdown);
         });
-        console.log(dropdown);
     }
+
 
 })();
