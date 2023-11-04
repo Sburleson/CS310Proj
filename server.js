0c1f40f6e1ce79e02126a82f6b26fb4dca7223e8
Sburleson <simonb9th@gmail.com>
2023-11-04 14:59:52 -0400

    api chages

--- server.js
+++ server.js
@@ -6,19 +6,21 @@ const port = 8080;
 app.use(express.static('static'));
 
 // Read the residence hall data from the JSON file
-const residenceHallsData = JSON.parse(fs.readFileSync('./reshalls.json'));
+const residenceHallsData = JSON.parse(fs.readFileSync('ResHall_info.json'));
 
 // Define an endpoint for your residence halls
 app.get('/api/reshalls/:name', (req, res) => {
-  const { name } = req.params;
+  console.log("here");
+  const name = req.params;
 
   // Find the image path for the residence hall
-  const imagePath = residenceHallsData[name];
+  const imagePath = residenceHallsData.ResHalls[name].img;
 
   if (!imagePath) {
     res.status(404).json({ error: 'Residence hall not found' });
   } else {
     // Return the image path
+    console.log(imagePath);
     res.json({ imageUrl: imagePath });
   }
 });
