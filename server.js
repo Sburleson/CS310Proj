5ade354551ca735f335adf26aafeff151637c948
Sburleson <simonb9th@gmail.com>
2023-11-04 15:15:08 -0400

    wierd changes

--- server.js
+++ server.js
@@ -7,19 +7,19 @@ app.use(express.static('static'));
 
 // Read the residence hall data from the JSON file
 const residenceHallsData = JSON.parse(fs.readFileSync('ResHall_info.json'));
-
+console.log("json done");
 // Define an endpoint for your residence halls
 app.get('/api/reshalls/:name', (req, res) => {
-  console.log("here");
+  console.log("inside get");
   const name = req.params;
-
+  //FOR SOME REASON NAME IS AN OBJECT. SO I HAD TO DO NAME.NAME
+  console.log(name);
   // Find the image path for the residence hall
-  const imagePath = residenceHallsData.ResHalls[name].img;
+  const imagePath = residenceHallsData.ResHalls[name.name].img;
 
   if (!imagePath) {
     res.status(404).json({ error: 'Residence hall not found' });
   } else {
-    // Return the image path
     console.log(imagePath);
     res.json({ imageUrl: imagePath });
   }
